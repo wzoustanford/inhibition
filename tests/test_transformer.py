793d81b30bff11c6fffb3b2d839914c099c9adcd
JEN6YT <71663327+JEN6YT@users.noreply.github.com>
2025-03-14 21:48:07 -0400

    device

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -9,9 +9,11 @@ from pathlib import Path
 def main():
     pl.seed_everything(42)
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     # get vocabulary size
     transformer = LightningTransformer(vocab_size=17546150, ninp=200, nhead=2, nhid=200, nlayers=3)
-
+    transformer = transformer.to(device)
     """
     The Trainer automatically calls the appropriate methods in your LightningModule. 
     When trainer.fit(model) is called, PyTorch Lightning will:
@@ -31,7 +33,7 @@ def main():
 
     trainer = Trainer(
         max_epochs=10,
-        accelerator="gpu",  # use 'gpu' if available; otherwise, use 'cpu'
+        accelerator=device,  # use 'cuda' if available; otherwise, use 'cpu'
         devices=1,          # number of GPUs, if available
         log_every_n_steps=100,
         logger=CSVLogger("csv_logs", name="transformer") # save logs to csv file in ogs/transformer/ directory.
